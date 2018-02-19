9fd8eeba315bffb8aad6e69d7d705a3f87f020e6
Unknown <multi.flexi@seznam.cz>
2018-02-19 15:52:29 +0100

    patching kernel

---
+++ kernel.sh
@@ -0,0 +1,8 @@
+git clone https://github.com/MarvellEmbeddedProcessors/linux-marvell
+cd linux-marvell
+git checkout linux-4.4.52-armada-17.10
+patch -p1 -i /home/jaroslav/EIT-Master_Thesis_project/patch-1
+export ARCH=arm64
+export CROSS_COMPILE=aarch64-linux-gnu-
+make mvebu_v8_lsp_defconfig
+make
