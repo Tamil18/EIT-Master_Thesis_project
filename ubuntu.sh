7875814742b3567c3459054ad663f9a7b0609ab3
Unknown <multi.flexi@seznam.cz>
2018-02-20 11:05:54 +0100

    minor

--- ubuntu.sh
+++ ubuntu.sh
@@ -10,5 +10,6 @@ sudo cp ~/linux-marvell/arch/arm64/boot/Image rootfs/boot/
 sudo cp ~/linux-marvell/arch/arm64/boot/dts/marvell/armada-8040-mcbin.dtb rootfs/boot/
 sudo tar -cjvf rootfs.tar.bz2 -C rootfs/ .
 sudo chown $LOGNAME rootfs.tar.bz2
+sudo umount temp/
 
 
