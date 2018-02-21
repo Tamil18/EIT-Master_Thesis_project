d7bd4c0ba379db284c037ea1564b4a47e520f630
Unknown <multi.flexi@seznam.cz>
2018-02-21 15:10:51 +0100

    update of toolchain

--- toolchain.sh
+++ toolchain.sh
@@ -3,6 +3,6 @@ sudo apt install -y ubuntu-dev-tools sed make binutils build-essential gcc g++ b
 cd ~/
 mkdir -p toolchain
 cd toolchain/
-wget https://releases.linaro.org/components/toolchain/binaries/5.3-2016.05/aarch64-linux-gnu/gcc-linaro-5.3.1-2016.05-x86_64_aarch64-linux-gnu.tar.xz
+wget https://releases.linaro.org/components/toolchain/binaries/5.5-2017.10/aarch64-linux-gnu/gcc-linaro-5.5.0-2017.10-x86_64_aarch64-linux-gnu.tar.xz
 tar -xvf gcc-linaro-5.3.1-2016.05-x86_64_aarch64-linux-gnu.tar.xz
-export PATH=$PATH:~/toolchain/gcc-linaro-5.3.1-2016.05-x86_64_aarch64-linux-gnu/bin
+export CROSS_COMPILE=/opt/gcc-linaro-5.5.0-2017.10-x86_64_aarch64-linux-gnu/bin/aarch64-linux-gnu-
